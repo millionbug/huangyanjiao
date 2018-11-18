970b7ebb707c66da45ede8871e4b3e3008db90c4
杨国栋 <yangguodong03@meituan.com>
2018-11-18 15:00:42 +0800

    项目开始 api servere 3000

--- server/nodeApp.js
+++ server/nodeApp.js
@@ -1,31 +1,22 @@
-// let {Application} = require('./module/app');
-// let Router = require('./module/router');
+let {Application} = require('./module/app');
+let Router = require('./module/router');
 
-// let fs = require('fs');
+let fs = require('fs');
 
-// let router = new Router;
-// let app = new Application();
-
-
-// router.use('/', (ctx, next) => {
-//   console.log(ctx.req.url, 'request path=========')
-//   ctx.body = fs.readFileSync(process.cwd() + '/dist/index.html').toString();
-//   next();
-// })
-// app.use(async (ctx, next) => {
-//   console.log(ctx.req.url, 'app.req.url===========')
-//   next();
-// })
-// app.use(router.routeMiddle())
+let router = new Router;
+let app = new Application();
 
 
-// app.listen('3000', _ => console.log('running in localhost:3000'));
-var http = require('http');
+router.use('/', (ctx, next) => {
+  console.log(ctx.req.url, 'request path=========')
+  ctx.body = fs.readFileSync(process.cwd() + '/dist/index.html').toString();
+  next();
+})
+app.use(async (ctx, next) => {
+  console.log(ctx.req.url, 'app.req.url===========')
+  next();
+})
+app.use(router.routeMiddle())
 
-http.createServer(function (request, response) {
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
+app.listen('3000', _ => console.log('running in localhost:3000'));
